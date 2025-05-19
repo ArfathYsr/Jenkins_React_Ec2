01299626e0c4990d1c33b7fc3177500e325a0e6f
Arfath_Ysr <arfath.916@outlook.com>
2025-05-19 16:20:41 +0530

    Update After_install.sh

--- scripts/After_install.sh
+++ scripts/After_install.sh
@@ -2,7 +2,6 @@
 echo "Installing and building app..."
 cp -r /tmp/react2-deploy/* /home/ubuntu/react2/
 cd /home/ubuntu/react2
-export PATH=$PATH:/home/ubuntu/.local/share/pnpm:/home/ubuntu/.npm-gl
-obal/bin
+export PATH=$PATH:/home/ubuntu/.local/share/pnpm:/home/ubuntu/.npm-global/bin
 pnpm install
 pnpm run build
