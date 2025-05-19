8bd39e92daf3bb36a2bdb0ae58ea4d8c37c61f74
Arfath_Ysr <arfath.916@outlook.com>
2025-05-19 16:20:08 +0530

    Update Application_Start.sh

--- scripts/Application_Start.sh
+++ scripts/Application_Start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Starting React app using PM2..."
-export PATH=$PATH:/home/ubuntu/.local/share/pnpm:/home/ubuntu/.npm-gl
+export PATH=$PATH:/home/ubuntu/.local/share/pnpm:/home/ubuntu/.npm-global/bin
+pm2 restart react2 || pm2 start pnpm --name react2 -- start
